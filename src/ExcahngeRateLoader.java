fff74ddffe50d0d76e36b647638a15e12de38c68
David Leon Ortega <david_lagu@hotmail.com>
2014-10-12 18:52:33 +0100

    Create loader classes

---
+++ src/ExcahngeRateLoader.java
@@ -0,0 +1,11 @@
+public class ExcahngeRateLoader {
+private final ExchangeRate exchangeRate;
+
+    public ExcahngeRateLoader(ExchangeRate exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public ExchangeRate load(){
+        return null;
+    }
+}
