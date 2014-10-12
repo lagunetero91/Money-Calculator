5caeabd0032d0227e3f9f88208371f5cd6d35d54
David Leon Ortega <david_lagu@hotmail.com>
2014-10-12 18:43:08 +0100

    Create Class Money and Currency.

---
+++ src/Currency.java
@@ -0,0 +1,24 @@
+public class Currency {
+private final int code;
+private final String name;
+private final String simbol;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSimbol() {
+        return simbol;
+    }
+
+    public Currency(int code, String name, String simbol) {
+        this.code = code;
+        this.name = name;
+        this.simbol = simbol;
+    }
+
+}
---
+++ src/Money.java
@@ -0,0 +1,18 @@
+public class Money {
+private final int amount;
+private final Currency my_currency;
+
+    public Money(int amount, Currency my_currency) {
+        this.amount = amount;
+        this.my_currency = my_currency;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Currency getMy_currency() {
+        return my_currency;
+    }
+
+}
