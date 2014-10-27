f16aa94d312a8a2cfd1148a7935d115dee13a590
usuario <usuario@10.22.143.38>
2014-10-27 13:48:11 +0000

    Añadido el hashmap para los currency set

--- src/Model/CurrencySet.java
+++ src/Model/CurrencySet.java
@@ -3,7 +3,8 @@ package Model;
 import java.util.HashMap;
 
 public class CurrencySet {
-    
+  
+    HashMap <String,Currency> conjunto = new HashMap<>();
     
     public Currency getCurrency(){
         return null;
