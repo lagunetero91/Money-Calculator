03a4b12b25d759a0bdbb52c5e2b29dd8d43e61f5
usuario <usuario@10.22.143.38>
2014-11-03 13:30:30 +0000

    Modificaciones varias

--- src/Persistence/CurrencySetLoader.java
+++ src/Persistence/CurrencySetLoader.java
@@ -1,18 +1,19 @@
 package Persistence;
-
-
 import Model.Currency;
+import Model.CurrencySet;
 
 public class CurrencySetLoader {
-private final Currency currency;
+private final CurrencySet currencyset;
 
-    public CurrencySetLoader(Currency currency) {
-        this.currency = currency;
+    public CurrencySetLoader(CurrencySet currencyset) {
+        this.currencyset = currencyset;
     }
     
-    public Currency Load(){
+    public CurrencySet Load(){
         Currency Euro = new Currency (1,"Euro","€");
         Currency Dolar = new Currency(2,"Dolar","$");
-        return null;
+        currencyset.addCurrency("Euro",Euro);
+        currencyset.addCurrency("Dolar",Dolar);
+        return currencyset;
     }
 }
