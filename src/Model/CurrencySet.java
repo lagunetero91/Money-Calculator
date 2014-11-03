261b15490923702ddf138941803c154e59d2ef01
usuario <usuario@10.22.143.38>
2014-11-03 13:30:54 +0000

    Modificaciones varias

--- src/Model/CurrencySet.java
+++ src/Model/CurrencySet.java
@@ -5,9 +5,14 @@ import java.util.HashMap;
 public class CurrencySet {
   
     HashMap <String,Currency> set = new HashMap<>();
-    
+    public CurrencySet(){
+        set.load();
+    }
     public Currency getCurrency(String key){
         return set.get(key);
         }
     
+    public void addCurrency(String key,Currency currency){
+        set.put(key, currency);
+    }
 }
