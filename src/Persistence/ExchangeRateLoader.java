9b822c0bbb9d5d88c548380a71002ce328b0048f
usuario <usuario@10.22.143.38>
2014-11-24 13:49:16 +0000

    Implementado el exchangeRateLoader pero falta acabar el processExcnageRate()

--- src/Persistence/ExchangeRateLoader.java
+++ src/Persistence/ExchangeRateLoader.java
@@ -2,16 +2,35 @@ package Persistence;
 
 
 import Model.ExchangeRate;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ExchangeRateLoader {
-private final ExchangeRate exchangeRate;
-
-    public ExchangeRateLoader(ExchangeRate exchangeRate) {
-        this.exchangeRate = exchangeRate;
+    private final Connection connection;
+    public ExchangeRateLoader(Connection connection) {
+        this.connection = connection;
     }
+    
 
-    public ExchangeRate load(){
-        
-        return null;
+    public ExchangeRate[] load(){
+        try{
+           return processQuery(connection.createStatement().executeQuery("SELECT * FROM exchange")); 
+        }catch(SQLException ex){
+        return new ExchangeRate [0];
     }
 }
+
+    private ExchangeRate[] processQuery(ResultSet resultSet) throws SQLException {
+        ArrayList<ExchangeRate> ExchangeRateSet = new ArrayList<>();
+        while(resultSet.next())
+            ExchangeRateSet.add(processExchangeRate(resultSet));
+        return ExchangeRateSet.toArray(new ExchangeRate[ExchangeRateSet.size()]);
+    }
+
+    private ExchangeRate processExchangeRate(ResultSet resultSet) {
+        return null;
+     
+    }
+        
