81671cbcdeec7cbb8327f9167cda3b77b9ccd521
usuario <usuario@10.22.143.38>
2014-11-24 13:26:02 +0000

    Implementado el currencySet Loader

--- src/Persistence/CurrencySetLoader.java
+++ src/Persistence/CurrencySetLoader.java
@@ -1,19 +1,39 @@
 package Persistence;
 import Model.Currency;
-import Model.CurrencySet;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CurrencySetLoader {
-private final CurrencySet currencyset;
+private final Connection connection;
 
-    public CurrencySetLoader(CurrencySet currencyset) {
-        this.currencyset = currencyset;
+    public CurrencySetLoader(Connection connection) {
+        this.connection = connection;
     }
     
-    public CurrencySet Load(){
-        Currency Euro = new Currency (1,"Euro","€");
-        Currency Dolar = new Currency(2,"Dolar","$");
-        currencyset.addCurrency("Euro",Euro);
-        currencyset.addCurrency("Dolar",Dolar);
-        return currencyset;
+    public Currency[] load(){
+        try{
+            return processQuery(connection.createStatement().executeQuery("SELECT * FROM currencys"));
+            
+        }catch(SQLException ex){
+            return new Currency[0];
+        }
     }
+    
+    private Currency[] processQuery(ResultSet resultSet) throws SQLException{
+        ArrayList<Currency> currencySet = new ArrayList<>();
+        while(resultSet.next())
+            currencySet.add(processPeron(resultSet));
+        return currencySet.toArray(new Currency[currencySet.size()]);
+    }
+
+    private Currency processPeron(ResultSet resultSet) throws SQLException {
+    return new Currency(
+        new Integer(resultSet.getString("code")),
+        resultSet.getString("name"),
+        resultSet.getString("symbol"));
+    
+    }
+    
 }
