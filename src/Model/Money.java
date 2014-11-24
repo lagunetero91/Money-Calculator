936381284215297601ed83e38b828cf19c20e1b9
usuario <usuario@10.22.143.38>
2014-11-24 13:58:33 +0000

    Arreglos varios + anterior commit

--- src/Model/Money.java
+++ src/Model/Money.java
@@ -5,19 +5,19 @@ import Model.Currency;
 
 public class Money {
 private final int amount;
-private final Currency my_currency;
+private final Currency currency;
 
-    public Money(int amount, Currency my_currency) {
+    public Money(int amount, Currency currency) {
         this.amount = amount;
-        this.my_currency = my_currency;
+        this.currency = currency;
     }
 
     public int getAmount() {
         return amount;
     }
 
-    public Currency getMy_currency() {
-        return my_currency;
+    public Currency getCurrency() {
+        return currency;
     }
 
 }
