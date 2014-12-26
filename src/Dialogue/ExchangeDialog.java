4a168cbe87b522bfe809ff3ae76609343e6f8d48
David <David@192.168.1.37>
2014-12-26 13:08:02 +0000

    Cambio ExchanngeDialog a interface

--- src/Dialogue/ExchangeDialog.java
+++ src/Dialogue/ExchangeDialog.java
@@ -1,24 +1,9 @@
 package Dialogue;
 
-import Model.Currency;
+
 import Model.Exchange;
-import Model.Money;
+    public interface ExchangeDialog {
+        
+        public Exchange getExchange();
 
-public class ExchangeDialog {
-    Currency currency;
-    Exchange exchange;
-    Money money;
-
-    public ExchangeDialog(Money money, Currency currency) {
-        this.money = money;
-        this.currency = currency;
-        exchange=new Exchange(money,currency);
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
-     
-    public void execute(){
-    }
     }
