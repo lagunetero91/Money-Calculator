f058d12a9c733266a46067b8a6fd389f32191819
David <David@192.168.1.37>
2015-01-18 16:13:26 +0000

    Create iterator in currencySet.

--- src/Model/CurrencySet.java
+++ src/Model/CurrencySet.java
@@ -1,10 +1,15 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class CurrencySet {
-    private final ArrayList <Currency> set = new ArrayList<>();
+public class CurrencySet implements Iterable<Currency>{
     
+    private final ArrayList <Currency> set;
+    
+    public CurrencySet() {
+        this.set = new ArrayList<>();
+    }
     
     public Currency getCurrency(int indice){
         return set.get(indice);
@@ -16,5 +21,10 @@ public class CurrencySet {
     
     public Currency[] toArray(){
         return set.toArray(new Currency[set.size()]);
+    }
+
+    @Override
+    public Iterator<Currency> iterator() {
+        return set.iterator();
     }
 }
