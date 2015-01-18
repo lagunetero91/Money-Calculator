a5c9273b67eb16137dacb458f553b6c84b5484f4
David <David@192.168.1.37>
2015-01-18 17:02:51 +0000

    Create mock version from ExchageRateLoader, i dont know hiow use database.

--- src/Persistence/ExchangeRateLoader.java
+++ src/Persistence/ExchangeRateLoader.java
@@ -1,41 +1,11 @@
+
 package Persistence;
 
-
-import Model.Currency;
+import Model.CurrencySet;
 import Model.ExchangeRate;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class ExchangeRateLoader {
-    private final Connection connection;
-    public ExchangeRateLoader(Connection connection) {
-        this.connection = connection;
-    }
-    
-
-    public ExchangeRate load(Currency from, Currency to){
-        try{
-           return processQuery(connection.createStatement().executeQuery(
-                   "SELECT * FROM exchangeRate"+
-                   "WHERE from=" +from+ 
-                   "AND to="+ to)); 
-        }catch(SQLException ex){
-        return null;
-    }
+   public ExchangeRate load(CurrencySet currencySet) {
+        return new ExchangeRate(currencySet.toArray()[0], currencySet.toArray()[1], 1.5);
+    } 
 }
-
-    private ExchangeRate processQuery(ResultSet resultSet) throws SQLException {
-        return processExchangeRate(resultSet);
-    }
-
-    private ExchangeRate processExchangeRate(ResultSet resultSet, Currency from, Currency to) throws SQLException {
-        return new ExchangeRate(
-        from,
-        to,
-        Integer.parseInt(resultSet.getString("rate")));
-     
-    }
-}
-        
